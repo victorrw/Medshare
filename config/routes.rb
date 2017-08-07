eb5c5715822ed668696e4bce8346e755dd6ca8fc
Victor Wilm <victorrw@msn.com>
2017-08-07 16:49:29 -0300

    Fixed routes 3.0

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :edit, :update, :show] do
     resources :meds
   end
+
   resources :shares, only: [:index, :create, :edit, :update]
 end
