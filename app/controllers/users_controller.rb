efbb0548103f60ffd1afba99f233c36a4d59e5df
Victor Wilm <victorrw@msn.com>
2017-08-07 18:40:07 -0300

    Basic controller skeleton, some things are still not working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,40 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
   def show
+  end
+
+  def private
+
+  def user_params
+    require(:user).permit(params[:name, :zipcode, :email, :address])
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
