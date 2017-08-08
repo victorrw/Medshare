b8959bb1506385b4534d8001e3fbb674ce211bd8
jose silveira <josesilveiraarquiteto@gmail.com>
2017-08-08 12:17:28 -0300

    frontend

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :users, only: [:new, :create, :edit, :update, :show] do
-    resources :meds
-  end
 
+  end
+  resources :meds
   resources :shares, only: [:index, :create]
 end
