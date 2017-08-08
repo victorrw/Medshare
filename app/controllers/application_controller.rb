7f4046880e704ca258616a36e9d5cd5b188534bf
Claudio Botelho <claudio.botelho@smartificar.com.br>
2017-08-08 11:18:22 -0300

    facebook connect prep with TA's hints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,15 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception #???
+  before_action :authenticate_user! # before any action of any controller (that's not in devise), check if user is logged in
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # protect_from_forgery
-  # before_action :authenticate_user!
 
   # skip_before_action :authenticate_user!, only: :home
   # def home
   # end
 
   def configure_permitted_parameters
-    # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :zipcode])
-
-    # For additional in app/views/devise/registrations/edit.html.erb
-    # devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 
 end
