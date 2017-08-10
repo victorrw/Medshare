32eed0050e3c9d60dec570cdd20e250a657e3506
Claudio Botelho <claudio.botelho@smartificar.com.br>
2017-08-10 19:01:14 -0300

    added status to shares, please run db:migrate

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,11 +3,22 @@ module UsersHelper
     current_user.facebook_picture_url || "no-avatar.gif"
   end
 
+  def current_user_meds_available
+    @meds.where(share_id: nil)
+  end
+
   def current_user_shares_waiting_on_donor
-    @shares_waiting = Share.where(taker_id: current_user.id)
+    shares_waiting = Share.where(taker_id: current_user.id)
   end
 
   def current_users_shares_to_approve
-    @shares_to_approve = Share.where(taker_id: current_user.id)
+    shares_to_approve = []
+    @shares.each do |share|
+      if share.giver == current_user
+      shares_to_approve << share
+      end
+    end
+    shares_to_approve
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170810152159) do
+ActiveRecord::Schema.define(version: 20170810210835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20170810152159) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "taker_id"
+    t.string   "status"
     t.index ["taker_id"], name: "index_shares_on_taker_id", using: :btree
   end
 
