527da4ca42e783dd1ac99bb9d988473155ad4dca
Claudio Botelho <claudio.botelho@smartificar.com.br>
2017-08-11 02:16:00 -0300

    user story ready. All share status implemented and operating

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,21 +4,34 @@ module UsersHelper
   end
 
   def current_user_meds_available
-    @meds.where(share_id: nil)
+    meds_available = @meds.where(share_id: nil)
   end
 
   def current_user_shares_waiting_on_donor
-    shares_waiting = Share.where(taker_id: current_user.id)
+    shares_waiting = Share.where(taker_id: current_user.id).where(status: "requested")
   end
 
   def current_users_shares_to_approve
     shares_to_approve = []
     @shares.each do |share|
-      if share.giver == current_user
+      if share.giver == current_user && share.status == "requested"
       shares_to_approve << share
       end
     end
     shares_to_approve
   end
 
+  def current_users_shares_sent
+    shares_sent = []
+    @shares.each do |share|
+      if share.giver == current_user && share.status == "sent"
+      shares_sent << share
+      end
+    end
+    shares_sent
+  end
+
+  def current_users_meds_received
+    meds_received = Share.where(taker_id: current_user.id).where(status: "sent")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, only: [:show]
   resources :meds
-  resources :shares, only: [:new, :create]
+  resources :shares, only: [:new, :create, :edit, :update]
   resources :users, only: [:show, :edit, :update]
 end
