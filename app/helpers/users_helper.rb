4ae0de319b01eb7058622b1c714d7533de68bfeb
Claudio Botelho <claudio.botelho@smartificar.com.br>
2017-08-15 11:09:04 -0300

    history ordered descending by order date

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,10 +5,12 @@ module UsersHelper
 
   def current_user_meds_available
     meds_available = @meds.where(share_id: nil)
+    sort_reverse(meds_available)
   end
 
   def current_user_shares_waiting_on_donor
     shares_waiting = Share.where(taker_id: current_user.id).where(status: "requested")
+    sort_reverse(shares_waiting)
   end
 
   def current_users_shares_to_approve
@@ -18,25 +20,33 @@ module UsersHelper
       shares_to_approve << share
       end
     end
-    shares_to_approve
+    sort_reverse(shares_to_approve)
   end
 
   def current_users_shares_sent
     shares_sent = []
     @shares.each do |share|
-      if share.giver == current_user && share.status == "sent" || && share.status == "received"
+      if share.giver == current_user && share.status == "sent" || share.status == "received"
       shares_sent << share
       end
     end
-    shares_sent
+    sort_reverse(shares_sent)
   end
 
   def current_users_meds_sent
     meds_sent = Share.where(taker_id: current_user.id).where(status: "sent")
+    sort_reverse(meds_sent)
   end
 
   def current_users_meds_received
     meds_received = Share.where(taker_id: current_user.id).where(status: "received")
+    sort_reverse(meds_received)
   end
+
+  private
+
+    def sort_reverse(shares)
+      shares.sort.reverse
+    end
 
 end
